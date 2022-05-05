6716b0d0e78e47005d4c3e5fbf274a773cbcdfc0
omar_kandil <93253683+OmarKandil-dw@users.noreply.github.com>
2022-05-05 15:50:44 +0000

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import User from './User';
+ReactDOM.render(
+    <div>
+<User nom ="omar" prenom="kandil" description=""/>,
+<User nom ="houda" prenom="sidi ammi" description=""/>,
+<User nom ="dd" prenom="" description=""/>,
+</div>,
+document.getElementById('root'));
